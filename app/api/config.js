7ab632390e6d983975562538be53bb470cee774f
tim2504 <102034439+fwegtbbgb@users.noreply.github.com>
2024-05-29 21:04:27 +0500

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://pindifrontend.nomoredomainswork.ru/api";
+export const BASE_URL = "https://gamesforall.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
