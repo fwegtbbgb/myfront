e2b34eb78f54f2516f46e3daf0b5e3bf0f4214ec
tim2504 <102034439+fwegtbbgb@users.noreply.github.com>
2024-05-29 21:20:24 +0500

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://gamesforall.nomoredomainswork.ru";
+export const BASE_URL = "https://gamesforall.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
